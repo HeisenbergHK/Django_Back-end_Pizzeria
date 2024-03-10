0c5f4f73e63e1f124e42430cbcffcd26f198601b
HeisenbergHK <92860076+HeisenbergHK@users.noreply.github.com>
2024-03-10 15:52:36 +0330

    Fixing the database (Everything is working!)

--- making_pizza/admin.py
+++ making_pizza/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pizza, Size, Crust, Topping, Type
+
+admin.site.register(Pizza)
+admin.site.register(Type)
+admin.site.register(Topping)
+admin.site.register(Crust)
+admin.site.register(Size)
--- making_pizza/models.py
+++ making_pizza/models.py
@@ -3,13 +3,15 @@ from django.db import models
 class Type(models.Model):
     name = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(null=True)
 
     def __str__(self):
-        return f'{self.name} with re cost of {self.cost}'
+        return f'{self.name} with the base price of {self.cost}'
 
 class Topping(models.Model):
     name = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(null=True)
 
     def __str__(self):
         return f'{self.name} with the cost of {self.cost}'
@@ -17,6 +19,7 @@ class Topping(models.Model):
 class Crust(models.Model):
     name = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(null=True)
 
     def __str__(self):
         return f'{self.name} with the cost of {self.cost}'
@@ -28,6 +31,7 @@ class Size(models.Model):
     def __str__(self):
         return f'{self.name} with the ratio of {self.ratio}'
 
+
 class Pizza(models.Model):
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
     extra_topping = models.ManyToManyField(Topping)
@@ -36,5 +40,24 @@ class Pizza(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     data_added = models.DateTimeField(auto_now_add=True)
 
+    def topping_to_string(self):
+        topping = []
+        for top in self.extra_topping.all():
+            topping.append(top.name)
+
+        if len(topping) == 1:
+            topping_str = topping[0]
+            return topping_str
+        else:
+            topping_str = ''
+            for i in range(len(topping)):
+                topping_str += topping[i]
+                if i < len(topping)-2:
+                    topping_str += ', '
+                elif i < len(topping)-1:
+                    topping_str += ' and '
+            return topping_str
+
+
     def __str__(self):
-        return f'a {self.type} pizza.'
+        return f'A {self.type.name} pizza with {self.topping_to_string()} on top.'
