14f65b79cd4ca2752670f8205f643c2f4616c394
HeisenbergHK <92860076+HeisenbergHK@users.noreply.github.com>
2024-03-11 16:28:24 +0330

    Ready to deploy to Platform.sh

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -133,3 +133,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'making_pizza:index'
 LOGOUT_REDIRECT_URL = 'making_pizza:index'
 LOGIN_URL = 'accounts:login'
+
+# platform.sh setting.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+            },
+        }
