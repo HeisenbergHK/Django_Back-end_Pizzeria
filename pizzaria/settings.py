e35a1a46fc8c231a7337040ba2d7d16d5a06cd73
HeisenbergHK <92860076+HeisenbergHK@users.noreply.github.com>
2024-07-17 09:40:33 +0330

    migration from sqlite to Postgres

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'pizzaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pizzaria',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
